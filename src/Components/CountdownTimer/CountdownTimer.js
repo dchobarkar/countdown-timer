1310c7c7b39b5b73296e1cab471e083746e09009
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-12 10:00:14 +0530

    complete initial setup and style

---
+++ src/Components/CountdownTimer/CountdownTimer.js
@@ -0,0 +1,18 @@
+import "./CountdownTimer.css";
+
+const CountdownTimer = () => {
+  return (
+    <div className="countdown-timer">
+      <span>00</span>
+      <span>Days</span>
+      <span>00</span>
+      <span>Hours</span>
+      <span>00</span>
+      <span>Minutes</span>
+      <span>00</span>
+      <span>Seconds</span>
+    </div>
+  );
+};
+
+export default CountdownTimer;
